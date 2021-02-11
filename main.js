b64a7b084ec9cba6f8502a8b0a1a86d442b61e9e
Sayantan Sinharay <s.sinharay.coder451@gmail.com>
2021-02-12 00:32:31 +0530

    minor bug fix and added a new feature

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
+const synth = window.speechSynthesis;
 const button = document.querySelector(".New-joke");
 const jokeText = document.querySelector(".joke-content");
-
+const speakJokes = document.querySelector(".Speak");
 button.addEventListener("click", getJoke);
 document.addEventListener("readystatechange", function (event) {
     if (event.target.readyState === "interactive") getJoke();
@@ -13,4 +14,16 @@ async function getJoke() {
     });
     const jokeObj = await jokeData.json();
     jokeText.innerHTML = jokeObj.joke;
+}
+function speakJoke() {
+    speakJokes.addEventListener("click", function () {
+        const utterThis = new SpeechSynthesisUtterance(jokeText.textContent);
+        utterThis.pitch = 1;
+        utterThis.rate = 1;
+        utterThis.voice = synth.getVoices()[1];
+        synth.speak(utterThis);
+    });
+}
+if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = speakJoke;
 }
